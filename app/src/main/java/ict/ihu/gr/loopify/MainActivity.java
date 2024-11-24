865e0d7a93ae1b8ff6f0d677a5fcd5b5d79b03e4
Petros Gatsos <petrosgat@hotmail.com>
2024-11-24 02:02:30 +0200

    fix : Initial Commit

--- app/src/main/java/ict/ihu/gr/loopify/MainActivity.java
+++ app/src/main/java/ict/ihu/gr/loopify/MainActivity.java
@@ -1,20 +1,22 @@
 package ict.ihu.gr.loopify;
 
-import static java.security.AccessController.getContext;
-
 import android.app.AlertDialog;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Intent;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
-import android.os.Looper;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
-
-import java.lang.reflect.Type;
-import java.util.List;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.result.ActivityResult;
 import androidx.activity.result.ActivityResultCallback;
@@ -29,18 +31,15 @@ import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
-import com.bumptech.glide.Glide;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
-import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
-import com.google.android.material.navigation.NavigationView;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.google.common.reflect.TypeToken;
+import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
@@ -48,42 +47,18 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.GoogleAuthProvider;
 import com.google.firebase.auth.UserProfileChangeRequest;
-
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
 import com.google.gson.JsonDeserializationContext;
 import com.google.gson.JsonDeserializer;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
 import com.google.gson.JsonParseException;
 
-import android.content.Intent;
-import android.os.Bundle;
-import androidx.appcompat.app.AppCompatActivity;
-
-import org.json.JSONArray;
 import org.json.JSONObject;
-import android.app.NotificationChannel;
-import android.app.NotificationManager;
-import android.os.Build;
-import android.widget.EditText;
-import android.widget.LinearLayout;
-import android.widget.TextView;
-import android.widget.Toast;
 
-import androidx.core.app.NotificationManagerCompat;
+import java.lang.reflect.Type;
+import java.util.List;
+
 import ict.ihu.gr.loopify.databinding.ActivityMainBinding;
-import ict.ihu.gr.loopify.ui.home.HomeFragment;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.net.HttpURLConnection;
-import java.net.URL;
-
-import ict.ihu.gr.loopify.Image;
-import ict.ihu.gr.loopify.Artist;
 
 //TEST TEST TEST TEST TEST TEST TEST TEST TEST
 
@@ -564,7 +539,7 @@ public class MainActivity extends AppCompatActivity implements ApiManager.ApiRes
         startMusicService("STOP");
         exoPlayerManager.release(); // Release MediaPlayer resources
     }
-
+//
     @Override
     public void onTracksFetched(String genre, List<Track> tracks) {
         // Handle the fetched tracks for each genre here
